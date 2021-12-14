c581055507c5b855323d2467d7a6026bb647b437
Lucas Santos <lucassantos@luizalabs.com>
2021-12-14 17:17:07 -0300

    fixing routes

--- hr-worker/src/main/java/com/lucas/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/lucas/hrworker/resource/WorkerResource.java
@@ -7,6 +7,7 @@ import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
     }
 
 }
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_WORKER (name, daily_income) VALUES ('LUCAS', 200.0)
+INSERT INTO TB_WORKER (name, daily_income) VALUES ('Joao', 200.0)
+INSERT INTO TB_WORKER (name, daily_income) VALUES ('Marcelo', 200.0)
