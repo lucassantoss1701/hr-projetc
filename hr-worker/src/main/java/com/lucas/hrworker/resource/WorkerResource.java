cd2440aab17a95587051a80dc6c80534f9069103
Lucas Santos <lucassantos@luizalabs.com>
2021-12-14 17:04:35 -0300

    find all hr-worker

---
+++ hr-worker/src/main/java/com/lucas/hrworker/resource/WorkerResource.java
@@ -0,0 +1,26 @@
+package com.lucas.hrworker.resource;
+
+
+import com.lucas.hrworker.entities.Worker;
+import com.lucas.hrworker.repositories.WorkerRepository;
+import org.hibernate.jdbc.Work;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
